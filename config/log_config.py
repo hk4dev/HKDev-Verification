cf8ce6332dfa9541e93b464270bb07c397215d0e
SH#9351 <perfect2315shlim@gmail.com>
2023-01-13 21:49:16 +0900

    fix(license): correct license information

--- config/log_config.py
+++ config/log_config.py
@@ -3,18 +3,18 @@ Version 3, 29 June 2007
 
 Copyright (c) 2021 gunyu1019
 
-PUBG BOT is free software: you can redistribute it and/or modify
+HKDev-Verification is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-PUBG BOT is distributed in the hope that it will be useful,
+HKDev-Verification is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with PUBG BOT.  If not, see <https://www.gnu.org/licenses/>.
+along with HKDev-Verification.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import logging
